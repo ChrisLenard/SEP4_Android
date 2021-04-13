5b2733a793dd90cb1c148856d08e7c07a8018972
Hibabc2000 <55843722+Hibabc2000@users.noreply.github.com>
2021-04-13 14:12:47 +0200

    ViewSpecimen first commit (requires styling)

---
+++ app/src/main/java/via/sep4/ViewSpecimen.java
@@ -0,0 +1,16 @@
+package via.sep4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewSpecimen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_specimen);
+    }
+
+
+}
