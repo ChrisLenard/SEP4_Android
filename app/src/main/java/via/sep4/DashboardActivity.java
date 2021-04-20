75d37cf2547290959886bee75283429073d8c677
Uldis Alksnis <csmccs1@gmail.com>
2021-04-20 17:42:17 +0200

    Dashboard add with Dialog, grid updates

--- app/src/main/java/via/sep4/DashboardActivity.java
+++ app/src/main/java/via/sep4/DashboardActivity.java
@@ -1,18 +1,17 @@
 package via.sep4;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.ViewCompat;
+import androidx.lifecycle.ViewModelProviders;
 
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.TypedValue;
+import android.text.InputType;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.ViewManager;
+import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -32,6 +31,7 @@ public class DashboardActivity extends AppCompatActivity {
     ArrayList<String> mushrooms = new ArrayList();
     ArrayList<Integer> tableRowIds = new ArrayList();
 
+    DashboardActivityViewModel dashboardActivityViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +42,22 @@ public class DashboardActivity extends AppCompatActivity {
         buttonAddMushroom = (ImageButton)findViewById(R.id.buttonAddMushroom);
         row1 = (TableRow)findViewById(R.id.dashboardRow1);
         tableLayout = (TableLayout)findViewById(R.id.table);
+
+        dashboardActivityViewModel = ViewModelProviders.of(this).get(DashboardActivityViewModel.class);
+        dashboardActivityViewModel.setData(this,row1,(ImageButton)findViewById(R.id.imageButton8),getDrawable(R.drawable.shroom),(TextView) findViewById(R.id.editTextTextPersonName4),(LinearLayout)findViewById(R.id.containerMushroom1));
+        dashboardActivityViewModel.addMushroom(new Mushroom("Latticed Stinkhorn"));
+        dashboardActivityViewModel.addMushroom(new Mushroom("Treehugger"));
+        dashboardActivityViewModel.addMushroom(new Mushroom("Puffball"));
+        dashboardActivityViewModel.addMushroom(new Mushroom("Indigo Milkcap"));
+        //dashboardActivityViewModel.reSetUpGrid() //Only for testing;
+        UpdateGrid();
+    }
+
+    public void UpdateGrid(){
+        tableLayout.removeAllViews();
+        for (TableRow row: dashboardActivityViewModel.getGrid()) {
+            tableLayout.addView(row);
+        }
     }
 
     public void NavigateDashboard(View view) {
@@ -57,71 +73,41 @@ public class DashboardActivity extends AppCompatActivity {
         startActivity(intentActivityAddMushroom);
     }
 
+
+    //On clicking + this brings a dialog with input
     public void AddMushroom(View view) {
-        TableRow row1 = (TableRow)findViewById(R.id.dashboardRow1);
-        String mushroomName = "Mushroom"+(mushrooms.size()+1);
-        //ImageButton for new shroom
-        ImageButton newShroomButton = new ImageButton(this);
-        ImageButton shroomButton = (ImageButton)findViewById(R.id.imageButton8);
-        ViewGroup.LayoutParams layoutShroomButton = shroomButton.getLayoutParams();
-        newShroomButton.setLayoutParams(layoutShroomButton);
-        newShroomButton.setMinimumWidth(toDensity(this,97));
-        newShroomButton.setMinimumHeight(toDensity(this,92));
-        newShroomButton.setBackgroundResource(R.drawable.shroom);
-        newShroomButton.setBackground(getDrawable(R.drawable.shroom));
-        newShroomButton.setClickable(true);
-        newShroomButton.setScaleType(ImageView.ScaleType.FIT_XY);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Mushroom Name");
 
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PERSON_NAME);
+        builder.setView(input);
 
-        //TextView for new shroom
-        TextView newShroomText = new TextView(this);
-        TextView shroomText = (TextView) findViewById(R.id.editTextTextPersonName4);
-        ViewGroup.LayoutParams layoutShroomText = shroomText.getLayoutParams();
-        newShroomText.setLayoutParams(layoutShroomText);
-        newShroomText.setMinEms(10);
-        newShroomText.setText(mushroomName);
-        newShroomText.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-        newShroomText.setTranslationZ(3);
-        newShroomText.setTextColor(Color.BLACK);
-        newShroomText.setId(ViewCompat.generateViewId());
-        newShroomText.setTextAppearance(R.style.TextAppearance_AppCompat_Medium);
-
-        //LinearLayout
-        LinearLayout newShroomContainer = new LinearLayout(this);
-        newShroomContainer.setTag("mushroom"+mushrooms.size());
-        LinearLayout linLayout = (LinearLayout)findViewById(R.id.containerMushroom1);
-        ViewGroup.LayoutParams lp = linLayout.getLayoutParams();
-        newShroomContainer.setLayoutParams(lp);
-        newShroomContainer.setOrientation(LinearLayout.VERTICAL);
-        newShroomContainer.setId(ViewCompat.generateViewId());
-        newShroomContainer.addView(newShroomButton);
-        newShroomContainer.addView(newShroomText);
-        if(mushrooms.size() % 3 == 0){
-            TableRow newRow = new TableRow(this);
-            newRow.setId(ViewCompat.generateViewId());
-            ViewGroup.LayoutParams tableRowLayout = row1.getLayoutParams();
-            newRow.setLayoutParams(tableRowLayout);
-            tableRowIds.add(newRow.getId());
-            newRow.addView(newShroomContainer);
-            tableLayout.addView(newRow);
-        }else{
-            TableRow rowToAdd = (TableRow)findViewById(tableRowIds.get((int)mushrooms.size()/3));
-            rowToAdd.addView(newShroomContainer);
-        }
-        mushrooms.add(mushroomName);
-        newShroomButton.setOnClickListener(new View.OnClickListener() {
+        builder.setPositiveButton("Add Mushroom", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(DialogInterface dialog, int which) {
+                dashboardActivityViewModel.addMushroom(new Mushroom(input.getText().toString()));
+                UpdateGrid();
             }
         });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 
-    public void RemoveMushroom(){
-        //int removalInThisRow = ((View)newShroomContainer.getParent()).getId();
-        //((ViewManager)newShroomContainer.getParent()).removeView(newShroomContainer);
-    }
-    public static int toDensity(final Context context, final float px) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, context.getResources().getDisplayMetrics());
+    public void RemoveMushroom(LinearLayout containerToRemove){
+        int removalInThisRow = ((View)containerToRemove.getParent()).getId();
+        int rowsToUpdate = tableRowIds.size()-tableRowIds.indexOf(removalInThisRow);
+
+        for(int i =rowsToUpdate;i<tableRowIds.size();i++){
+            TableRow rowToUpdate = (TableRow)findViewById(tableRowIds.get(i));
+        }
+        ((ViewManager)containerToRemove.getParent()).removeView(containerToRemove);
+
     }
 }
---
+++ app/src/main/java/via/sep4/Mushroom.java
@@ -0,0 +1,53 @@
+package via.sep4;
+
+import android.widget.LinearLayout;
+import android.widget.TableRow;
+
+import java.util.Objects;
+
+public class Mushroom {
+    private String name;
+    private TableRow parent;
+    private LinearLayout container;
+
+    public Mushroom(String name){
+        this.name = name;
+    }
+
+    public LinearLayout getContainer() {
+        return container;
+    }
+
+    public void setContainer(LinearLayout container) {
+        this.container = container;
+    }
+
+    public TableRow getParent() {
+        return parent;
+    }
+
+    public void setParent(TableRow parent) {
+        this.parent = parent;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mushroom mushroom = (Mushroom) o;
+        return Objects.equals(name, mushroom.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
