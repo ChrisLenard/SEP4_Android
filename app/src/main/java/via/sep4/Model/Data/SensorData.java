4ae910206e8aebe101465469f80c72dd94ba76f7
Hibabc2000 <55843722+Hibabc2000@users.noreply.github.com>
2021-04-21 07:35:29 +0200

    Added and started documenting backend classes, updated and configured build.gradle and manifest.

---
+++ app/src/main/java/via/sep4/Model/Data/SensorData.java
@@ -0,0 +1,10 @@
+package via.sep4.Model.Data;
+
+public class SensorData {
+    //todo
+    /**
+     * @author Kristóf Lénárd
+     * @version 1.0
+     * This class stores all saved sensor data at a given timestamp.
+     */
+}
---
+++ app/src/main/java/via/sep4/Model/WebHandler.java
@@ -0,0 +1,9 @@
+package via.sep4.Model;
+
+public class WebHandler {
+    /**
+     * @author Kristóf Lénárd
+     * @version 1.0
+     * This class connects the application data and the REST client.
+     */
+}
