3786dbbdd2305595a5801a59897675f0598f9fdf
Hibabc2000 <55843722+Hibabc2000@users.noreply.github.com>
2021-04-23 08:24:18 +0200

    ViewModel and Handler updates, configurations.

--- app/src/main/java/via/sep4/Model/PersistenceHandler.java
+++ app/src/main/java/via/sep4/Model/PersistenceHandler.java
@@ -1,9 +1,17 @@
 package via.sep4.Model;
 
+import via.sep4.Model.Data.SensorDataList;
+
 public class PersistenceHandler {
     /**
      * @author Kristóf Lénárd
      * @version 1.0
      * This class is responsible for implementing the Data Access Objects, supporting the LocalPersistence.
      */
+
+    public SensorDataList getAllSensorData(int specimenKey)
+    {
+        //TODO: write sensor data getter w/ Room
+        return null;
+    }
 }
---
+++ app/src/main/java/via/sep4/ViewSpecimenViewModel.java
@@ -0,0 +1,55 @@
+package via.sep4;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.net.InetAddress;
+
+import via.sep4.Model.Data.SensorDataList;
+import via.sep4.Model.PersistenceHandler;
+import via.sep4.Model.WebHandler;
+
+public class ViewSpecimenViewModel extends ViewModel
+{
+    /**
+     * @author Kristóf Lénárd
+     * @version 1.0
+     * This class is responsible for implementing UI background code.
+     */
+
+    private PersistenceHandler persistenceHandler;
+    private WebHandler webHandler;
+    private int specimenKey;
+    private MutableLiveData<SensorDataList> sensorLiveData;
+    //TODO: visualizer, visualizer data binding
+
+    public ViewSpecimenViewModel()
+    {
+        persistenceHandler = new PersistenceHandler();
+        webHandler = new WebHandler();
+        sensorLiveData = new MutableLiveData<>();
+        specimenKey = 0;
+        //TODO: write specimen key retriever
+    }
+
+    public MutableLiveData<SensorDataList> getSensorLiveData() {
+        return sensorLiveData;
+    }
+
+    private void setSensorLiveData()
+    {
+        sensorLiveData.setValue(getSensorData());
+    }
+
+    public SensorDataList getSensorData()
+    {
+        //TODO: write internet checker
+        return webHandler.getAllSensorData(specimenKey);
+    }
+
+    public SensorDataList getLocalSensorData()
+    {
+        return persistenceHandler.getAllSensorData(specimenKey);
+    }
+
+}
