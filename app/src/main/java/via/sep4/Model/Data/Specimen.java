cf4d724b557fa394199860d606167ec73d017347
Uldis Alksnis <csmccs1@gmail.com>
2021-04-26 06:27:50 +0200

    Retrofit HardwareAPI,SpecimenAPI,StatusAPI,UserAPI, WebClient

---
+++ app/src/main/java/via/sep4/Model/Data/Specimen.java
@@ -0,0 +1,85 @@
+package via.sep4.Model.Data;
+
+public class Specimen {
+    private int specimen_key;
+    private double planted_date; // This is double due to int max size - Swagger provides higher value than max int can hold
+    private String specimen_name;
+    private String specimen_type;
+    private String specimen_description;
+    private float desired_air_temperature;
+    private float desired_air_humidity;
+    private float desired_air_co2;
+    private int hardware_id;
+
+    public int getSpecimen_key() {
+        return specimen_key;
+    }
+
+    public void setSpecimen_key(int specimen_key) {
+        this.specimen_key = specimen_key;
+    }
+
+    public double getPlanted_date() {
+        return planted_date;
+    }
+
+    public void setPlanted_date(double planted_date) {
+        this.planted_date = planted_date;
+    }
+
+    public String getSpecimen_name() {
+        return specimen_name;
+    }
+
+    public void setSpecimen_name(String specimen_name) {
+        this.specimen_name = specimen_name;
+    }
+
+    public String getSpecimen_type() {
+        return specimen_type;
+    }
+
+    public void setSpecimen_type(String specimen_type) {
+        this.specimen_type = specimen_type;
+    }
+
+    public String getSpecimen_description() {
+        return specimen_description;
+    }
+
+    public void setSpecimen_description(String specimen_description) {
+        this.specimen_description = specimen_description;
+    }
+
+    public float getDesired_air_temperature() {
+        return desired_air_temperature;
+    }
+
+    public void setDesired_air_temperature(float desired_air_temperature) {
+        this.desired_air_temperature = desired_air_temperature;
+    }
+
+    public float getDesired_air_humidity() {
+        return desired_air_humidity;
+    }
+
+    public void setDesired_air_humidity(float desired_air_humidity) {
+        this.desired_air_humidity = desired_air_humidity;
+    }
+
+    public float getDesired_air_co2() {
+        return desired_air_co2;
+    }
+
+    public void setDesired_air_co2(float desired_air_co2) {
+        this.desired_air_co2 = desired_air_co2;
+    }
+
+    public int getHardware_id() {
+        return hardware_id;
+    }
+
+    public void setHardware_id(int hardware_id) {
+        this.hardware_id = hardware_id;
+    }
+}
--- app/src/main/java/via/sep4/Persistence/WebClient.java
+++ app/src/main/java/via/sep4/Persistence/WebClient.java
@@ -1,5 +1,8 @@
 package via.sep4.Persistence;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -10,16 +13,67 @@ public class WebClient {
      * This class is responsible for implementing the Retrofit library, enabling connections to the webservice.
      */
 
+    //URL for connection
+    private static final String BASE_URL = "https://app.swaggerhub.com/apis-docs/C4T4PHR4CT/SEP4";
+
     private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
-            .baseUrl("https://app.swaggerhub.com/apis-docs/C4T4PHR4CT/SEP4")
+            .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create());
 
     private static Retrofit retrofit = retrofitBuilder.build();
 
-    private static SpecimenAPI specimenAPI = retrofit.create(SpecimenAPI.class);
+    private static OkHttpClient.Builder httpClient
+            = new OkHttpClient.Builder();
+
+    private static HttpLoggingInterceptor logging
+            = new HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY);
+
+    private static SpecimenAPI specimenAPI = createService(SpecimenAPI.class,"auth-token");
+    private static UserAPI userAPI = createService(UserAPI.class,"auth-token");
+    private static HardwareAPI hardwareAPI = createService(HardwareAPI.class,"auth-token");
+    private static StatusAPI statusAPI = createService(StatusAPI.class,"auth-token");
+
+    public static <S> S createService(Class<S> serviceClass) {
+        if (!httpClient.interceptors().contains(logging)) {
+            httpClient.addInterceptor(logging);
+            retrofitBuilder.client(httpClient.build());
+            retrofit = retrofitBuilder.build();
+        }
+        return retrofit.create(serviceClass);
+    }
+
+    public static <S> S createService(Class<S> serviceClass, final String token) {
+        if (token != null) {
+            httpClient.interceptors().clear();
+            httpClient.addInterceptor( chain -> {
+                Request original = chain.request();
+                Request.Builder builder1 = original.newBuilder()
+                        .header("Authorization", token);
+                Request request = builder1.build();
+                return chain.proceed(request);
+            });
+            httpClient.addInterceptor(logging);
+            retrofitBuilder.client(httpClient.build());
+            retrofit = retrofitBuilder.build();
+        }
+        return retrofit.create(serviceClass);
+    }
 
     public static SpecimenAPI getSpecimenAPI()
     {
         return specimenAPI;
+    }
+
+    public static UserAPI getUserAPI() {
+        return userAPI;
+    }
+
+    public static HardwareAPI getHardwareAPI() {
+        return hardwareAPI;
+    }
+
+    public static StatusAPI getStatusAPI() {
+        return statusAPI;
     }
 }
